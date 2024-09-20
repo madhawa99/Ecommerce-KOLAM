c7c914c3f4fe3112604bc37cc08582b3a42609e0
Ishan Giwantha <ishangiwantha1234@gmail.com>
2024-09-20 22:24:13 +0530

    update versions

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -4,3 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+
+
+//redis is a key value store
+/**
+import Redis from "ioredis"
+
+const client = new Redis("rediss://default:AY2fAAIjcDE5ZGE4OGFmYjg3OGM0MzllOGI4OGE2ZTI5OWU2ZDgzYXAxMA@singular-ewe-36255.upstash.io:6379");
+await client.set('foo', 'bar');
+ */
