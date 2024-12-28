c26b4132186eb241970a55aae28b84bc22aa8b19
Ishan Giwantha <ishangiwantha1234@gmail.com>
2024-12-29 01:02:54 +0530

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -33,7 +33,7 @@ const userSchema = new mongoose.Schema(
 		],
 		role: {
 			type: String,
-			enum: ["customer", "admin","seller"],
+			enum: ["customer","seller", "admin"],
 			default: "customer",
 		},
 	},
